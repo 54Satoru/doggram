c44f44fcf9bf025ac25696ffe5a324245cf45cc0
54Satoru <satoru.pro.54@gmail.com>
2020-11-23 20:00:09 +0900

    'dogs'

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db2e7da7bf81cee1d183d4fb26a003044e14ba65816a8db08817b6d57d6d3c9b2519d12ec4d1b723a221f2939fb6c20cc6644177e9c3f6f4f6c0bea39777c33a'
