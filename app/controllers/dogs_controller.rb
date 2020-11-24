3b5d0cf3b1df4612281f06f2abcabe4c4445522f
54Satoru <satoru.pro.54@gmail.com>
2020-11-24 13:06:37 +0900

    finish

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,6 @@
 class DogsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @dogs = Dog.all
   end
@@ -14,18 +16,33 @@ class DogsController < ApplicationController
   def create
     @dog = Dog.new(dog_params)
     @dog.user_id = current_user.id
-    @dog.save
-    redirect_to dog_path(@dog)
+    if @dog.save
+      redirect_to dog_path(@dog), notice: '投稿に成功しました！'
+    else
+      render :new
+    end
   end
 
   def edit
     @dog = Dog.find(params[:id])
+    if @dog.user != current_user
+      redirect_to dogs_path, alert: '不正なアクセスです'
+    end
   end
 
   def update
     @dog = Dog.find(params[:id])
-    @dog.update(dog_params)
-    redirect_to dog_path(@dog)
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog), notice: '更新に成功しました！'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    dog = Dog.find(params[:id])
+    dog.destroy
+    redirect_to dogs_path
   end
 
   private
